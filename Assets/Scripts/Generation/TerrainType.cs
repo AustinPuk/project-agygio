8c85d76b71aef0aad13976113ad9fd1ef7acc3ad
Austin Pukasamsombut <vpukasam@UCSD.EDU>
2017-03-20 07:23:57 -0700

    Starting world generation

---
+++ Assets/Scripts/Generation/TerrainType.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainType : MonoBehaviour{
+
+    [Header("Game Objects")]
+    public GameObject[] trees;
+    public Vector2 treeRange;
+    public GameObject[] rocks;
+    public Vector2 rockRange;
+
+    public GameObject[] items;
+    public float[] itemChances; // Between 0 to 1
+    public GameObject[] enemies;
+    public float[] enemyChances;
+
+    [Header("Height Map Paramters")]
+    public int HEIGHT_MAP_POWER = 8;
+    public float HEIGHT_MAP_MAX = 10.0f;
+    public float HEIGHT_RANDOMNESS_SCALE = 100.0f;
+
+    [Header("Terrain Parameters")]
+    public int TERRAIN_RESOLUTION = 30;
+    public float TERRAIN_SIZE = 100.0f;
+    public float TERRAIN_SMOOTHNESS = 1.2f; // Lower to make rough    
+
+    [Header("Fixed Points")]
+    public List<Vector3> fixedPoints;
+}
---
+++ Assets/Scripts/Generation/WorldGenerator.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public static WorldGenerator instance;
+
+    // Handles overall world management, mainly global height lookup
+    // Also delegates terrain generation so that fixed points match
+
+    [SerializeField]
+    int seed;
+
+    [SerializeField]
+    bool saveFile = false;
+
+    [SerializeField]
+    List<TerrainGenerator> terrains;
+
+    private void Awake()
+    {
+        if (!instance)
+            instance = this;
+
+        if (seed != 0)
+            Random.InitState(seed);
+
+        GenerateWorld();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+   
+    /***************** Core Functions ************************/
+
+    private void GenerateWorld()
+    {
+        List<Vector3> fixedPoints = new Vector3(terrains[0].getSize(0), terrains[0].ty)
+        // Generate Center World        
+        terrains[0].GenerateTerrain();
+
+
+        // Generate Corner Terrains
+
+    }
+
+    public float HeightLookup(float x, float z)
+    {
+        // Returns the height at the given location, spanning all 9 terrains.
+        return 0; 
+    }
+
+    /*************** Generation Functions *********************/
+
+    private void GenerateTrees(float size, int numTrees, GameObject[] trees)
+    {
+        // TODO: Change parameters to allow offset to specific terrain are we are currently in
+        float middle = size / 2.0f;
+        float min_x = -middle;
+        float min_z = -middle;
+        float max_x = middle;
+        float max_z = middle;
+
+        for (int i = 0; i < numTrees; i++)
+        {
+            GameObject newTree = Instantiate(trees[Random.Range(0, trees.Length)]);
+
+            float x = Random.Range(min_x, max_x);
+            float z = Random.Range(min_z, max_z);
+            float y = HeightLookup(x, z);
+
+            newTree.transform.position = new Vector3(x, y, z);
+            newTree.transform.SetParent(this.transform);
+        }
+    }
+
+    private void GenerateRocks(float size, int numRocks, GameObject[] rocks)
+    {
+        // Similar to Generate Trees
+    }
+
+    private void GenerateItems(float size, int numItems, GameObject[] items, float[] itemChances)
+    {
+        // Generate random items scattered around the area.
+        // Types of terrains have different probabilties for each item
+    }
+
+    private void GenerateEnemies(float size, int numEnemies, GameObject[] enemies, float[] enemyChances)
+    {
+        // Generates random enemies. This will be more strict, a single terrain shouldn't usually have
+        // multiple types of enemies. Once enemies are spawned, they take on their own behaviours.
+        // Some enemies may disappear in the morning, then reappear at night.
+
+
+    }
+}
